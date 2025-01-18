34f588ead1c7d97b92fbdf7489ffd93e1d0aaf80
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2025-01-18 17:32:12 +1300

    addes sign up page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,24 +1,12 @@
-export default function LoginPage() {
-  async function handleSubmit(formData: FormData) {
-    'use server';
-    
-    const username = formData.get('username');
-    const email = formData.get('email');
-    const password = formData.get('password');
-    
-    // TODO: Implement your login logic here
-    console.log('Login attempt:', { username, email, password });
-  }
+"use client"
+import { handleSignUpSubmit } from "../actions";
 
+
+
+export default function LoginPage() {
   return (
     <div>
-      <h1>Login</h1>
-      <form action={handleSubmit} className="flex flex-col gap-2">
-        <input type="text" name="username" placeholder="Username" />
-        {/* <input type="text" name="email" placeholder="Email" /> */}
-        <input type="password" name="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+
     </div>
   );
 }
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 import { eq } from 'drizzle-orm';
 import { usersTablePG } from './schema';
-  
+import type { InferSelectModel } from "drizzle-orm";
+
+
 const db = drizzle(process.env.DATABASE_URL!)
 
 export default db;
