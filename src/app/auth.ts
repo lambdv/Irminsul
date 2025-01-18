548ab6b7a69fdee6acf9cae0f1dbb19c9189dbf8
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2025-01-18 21:00:38 +1300

    added secret env to provider

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -4,6 +4,7 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import db from "@/db/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   adapter: DrizzleAdapter(db),
   providers: [
     GithubProvider({
