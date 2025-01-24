f292badb3963ad6e13a1c22335bc069ff0963989
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2025-01-25 11:57:57 +1300

    bug fix: table of content incorrect positioning

--- src/components/navigation/LeftSidenav.tsx
+++ src/components/navigation/LeftSidenav.tsx
@@ -2,8 +2,9 @@ import RightSidenavCSS from '@/components/navigation/rightsidenav.module.css'
 
 export default function LeftSidenav(props) {
   return (
-      <nav className={`${RightSidenavCSS.leftsidenav} ${props.className}`} >
-        {props.children}
-      </nav>
-  )
+      // <nav className={`${RightSidenavCSS.leftsidenav} ${props.className}`} >
+      //   {props.children}
+      // </nav>
+    <></>
+    )
 }
