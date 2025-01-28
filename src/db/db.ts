3e09e4b3834115dc18441a4512a800fd9cf2d66d
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2025-01-29 08:53:05 +1300

    removed broken links to schemas

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 import { eq } from 'drizzle-orm';
-import { usersTablePG } from './schema';
 import type { InferSelectModel } from "drizzle-orm";
 
 const db = drizzle(process.env.DATABASE_URL!)
