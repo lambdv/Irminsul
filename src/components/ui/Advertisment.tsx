5c4e049aa95b5066f5840a794fd4f3330108b20e
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2025-02-07 17:45:01 +1300

    fixed manual adverisement component for next

--- src/components/ui/Advertisment.tsx
+++ src/components/ui/Advertisment.tsx
@@ -1,41 +1,35 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import React, { useEffect } from "react";
+import Script from "next/script";
 
-export default function Advertisment(props: {
-    type: "card" | "banner" | "in-article"
-}) {
+export default function Advertisment(props: { type: "card" | "banner" | "in-article" }) {
+  useEffect(() => {
+    try {
+      (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+      (window as any).adsbygoogle.push({});
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }, []);
 
-    useEffect(() => {
-        try {
-          ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-        } 
-        catch (error: any) { console.log(error.message);}
-      }, []);
+  return (
+    <>
+      {/* Load AdSense script only once */}
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
 
-    return (
-        <div id="advertisment" className='flex flex-box justify-center items-center' style={{
-            // backgroundColor: "#161616",
-            borderRadius: "5px",
-            padding: "10px",
-            margin: "10px",
-            width: "100%",
-            height: "auto",
-            color: "#5f5f5f",
-        }}>
-            {/* <p className='text-sm '>adge</p> */}
-            {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1739492299738628"crossOrigin="anonymous"></script> */}
-
-            <ins className="adsbygoogle"
-                style={{display: "block"}}
-                data-ad-client="ca-pub-1739492299738628"
-                data-ad-slot="9046274057"
-                data-ad-format="auto"
-                data-full-width-responsive="true">    
-            </ins>
-
-            {/* <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script> */}
-        </div>
-    )
+      {/* AdSense ad */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1739492299738628"
+        data-ad-slot="9046274057"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </>
+  );
 }
